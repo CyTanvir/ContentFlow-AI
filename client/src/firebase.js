58741b773950943840446162adb18c3a9fc4fd74
DravenWilson1 <dravenwilson@icloud.com>
2026-02-08 23:36:15 -0500

    Created add content feature
    Text editor allows upload and download to database as well as edit and delete

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDNYV8rYI9D6SY9HIvXvXrWSysALDrp_ak",
@@ -9,4 +10,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig); 
 export const auth = getAuth(app);
-
+export const db = getFirestore(app);
