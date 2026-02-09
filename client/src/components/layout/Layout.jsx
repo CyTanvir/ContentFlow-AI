fe1f9dda0f9fb71fc33a07a846061fa15f24aa46
abdalaa20 <hl4371@wayne.edu>
2026-02-08 23:32:55 -0500

    Update to the Welcome dashboard
    Displays name

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,14 +1,13 @@
 import Sidebar from "./Sidebar";
 import Topbar from "./Topbar";
 import "../styles/layout.css";
-
 const Layout = ({ children, onLogout, displayName }) => {
   return (
     <div className="app-layout">
       <Sidebar />
 
       <div className="main-area">
-        <Topbar onLogout={onLogout} displayName={displayName} />
+      <Topbar onLogout={onLogout} displayName={displayName} />
         <div className="content-area">{children}</div>
       </div>
     </div>
